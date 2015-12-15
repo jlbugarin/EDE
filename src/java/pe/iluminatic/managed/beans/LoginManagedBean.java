27b236c81a53a8ced512a1e61211288bb1dc6421
Jose Luis Bugarin <jlbugarin@gmail.com>
2015-12-15 15:17:01 -0500

    actualizacion entidades para que considere nombre en mayusculas. Corregido EDEEVAL

--- src/java/pe/iluminatic/managed/beans/LoginManagedBean.java
+++ src/java/pe/iluminatic/managed/beans/LoginManagedBean.java
@@ -10,13 +10,10 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import pe.iluminatic.dao.entidades.UserRoles;
 import pe.iluminatic.dao.entidades.Users;
 
 /**
